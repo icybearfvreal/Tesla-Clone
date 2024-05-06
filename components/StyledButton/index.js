3c415339e30a7cb465a6b3ba531d7c11fb647067
Icybearfvreal <arcader4g@gmail.com>
2024-05-06 20:02:34 +0100

    Car Item component

---
+++ components/StyledButton/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import {View, Text, Pressable} from 'react-native';
+import styles from './style';
+
+const StyledButton = (props) => {
+
+    const type = props.type;
+
+    const backgroundColor = type == 'primary' ? 'black' : 'white';
+
+    return (
+       <View style={styles.container}>
+        <Pressable 
+        style={[styles.button, {backgroundColor: backgroundColor}]}
+        onPress={() => {
+            console.warn('Hey There')
+        }}>
+        <Text style={styles.text}>Custom Order</Text>
+        </Pressable>
+       </View>  
+    );
+};
+
+export default StyledButton;
